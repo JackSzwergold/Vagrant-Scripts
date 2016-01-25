9880124a190c335712f4984d5f481adc0d9c8db1
Jack Szwergold <JackSzwergold@gmail.com>
2016-01-25 18:04:57 -0500

    Adding some MySQL stuff to the mix.

--- provision.sh
+++ provision.sh
@@ -14,13 +14,13 @@ export DEBIAN_FRONTEND=noninteractive
 echo -e "PROVISIONING: User and group related stuff.\n"
 
 # Create the 'www-readwrite' group.
-sudo groupadd -f www-readwrite
+sudo -E groupadd -f www-readwrite
 
 # Set the Vagrant users main group to be the 'www-readwrite' group.
-sudo usermod -g www-readwrite vagrant
+sudo -E usermod -g www-readwrite vagrant
 
 # Add the user to the 'www-readwrite' group:
-sudo adduser --quiet vagrant www-readwrite
+sudo -E adduser --quiet vagrant www-readwrite
 
 ######################################################################################
 # Date and Time
@@ -29,27 +29,27 @@ sudo adduser --quiet vagrant www-readwrite
 echo -e "PROVISIONING: Set the date and time stuff.\n"
 
 # Sync with the time/date server.
-sudo ntpdate -u ntp.ubuntu.com
+sudo -E ntpdate -u ntp.ubuntu.com
 
 # Set the time zone data.
 # debconf-set-selections <<< "tzdata tzdata/Areas select America"
 # debconf-set-selections <<< "tzdata tzdata/Zones/America select New_York"
-# sudo dpkg-reconfigure tzdata
+# sudo -E dpkg-reconfigure tzdata
 TIMEZONE="America/New_York";
 TIMEZONE_PATH="/etc/timezone";
 if [ "${TIMEZONE}" != $(cat "${TIMEZONE_PATH}") ]; then
-  sudo echo "${TIMEZONE}" > "${TIMEZONE_PATH}";
-  sudo dpkg-reconfigure -f noninteractive tzdata;
+  sudo -E echo "${TIMEZONE}" > "${TIMEZONE_PATH}";
+  sudo -E dpkg-reconfigure -f noninteractive tzdata;
 fi
 
 # Edit the 'sources.list' to enable partner package updates.
 SOURCES_LIST="/etc/apt/sources.list";
 if [ -f "${SOURCES_LIST}" ]; then
-  sudo sed -i '/^#.*deb.*partner$/s/^# //g' "${SOURCES_LIST}"
+  sudo -E sed -i '/^#.*deb.*partner$/s/^# //g' "${SOURCES_LIST}"
 fi
 
 # Install Avahi daemon stuff.
-sudo aptitude install -y --assume-yes -q avahi-daemon avahi-utils
+sudo -E aptitude install -y --assume-yes -q avahi-daemon avahi-utils
 
 ######################################################################################
 # Sysstat
@@ -58,16 +58,16 @@ sudo aptitude install -y --assume-yes -q avahi-daemon avahi-utils
 echo -e "PROVISIONING: Sysstat related stuff.\n"
 
 # Install 'sysstat'.
-sudo aptitude install -y --assume-yes -q sysstat
+sudo -E aptitude install -y --assume-yes -q sysstat
 
 # Enable 'sysstat'.
 SYSSTAT_CONFIG_PATH="/etc/default/sysstat";
 if [ -f "${SYSSTAT_CONFIG_PATH}" ]; then
-  sudo sed -i 's/ENABLED="false"/ENABLED="true"/g' "${SYSSTAT_CONFIG_PATH}";
+  sudo -E sed -i 's/ENABLED="false"/ENABLED="true"/g' "${SYSSTAT_CONFIG_PATH}";
 fi
 
 # Restart 'sysstat'.
-sudo service sysstat restart
+sudo -E service sysstat restart
 
 ######################################################################################
 # Generic Tools
@@ -76,7 +76,7 @@ sudo service sysstat restart
 echo -e "PROVISIONING: Installing a set of generic tools.\n"
 
 # Install generic tools.
-sudo aptitude install -y --assume-yes -q \
+sudo -E aptitude install -y --assume-yes -q \
   dnsutils traceroute nmap bc htop finger curl whois rsync lsof \
   iftop figlet lynx mtr-tiny iperf nload zip unzip attr sshpass \
   dkms mc elinks ntp dos2unix p7zip-full nfs-common imagemagick \
@@ -89,8 +89,8 @@ sudo aptitude install -y --assume-yes -q \
 echo -e "PROVISIONING: Installing the locate tool and updating the database.\n"
 
 # Install and update the locate database.
-sudo aptitude install -y --assume-yes -q locate
-sudo updatedb
+sudo -E aptitude install -y --assume-yes -q locate
+sudo -E updatedb
 
 ######################################################################################
 # Compiler
@@ -99,7 +99,7 @@ sudo updatedb
 echo -e "PROVISIONING: Installing the core compiler tools.\n"
 
 # Install the core compiler and built options.
-sudo aptitude install -y --assume-yes -q build-essential
+sudo -E aptitude install -y --assume-yes -q build-essential
 
 ######################################################################################
 # Git
@@ -109,11 +109,11 @@ echo -e "PROVISIONING: Installing Git and related stuff.\n"
 
 # Install Git via PPA.
 if ! grep -q -s "git-core" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-  sudo aptitude install -y --assume-yes -q python-software-properties
-  sudo add-apt-repository -y ppa:git-core/ppa
-  sudo aptitude update -y --assume-yes -q
-  sudo aptitude upgrade -y --assume-yes -q
-  sudo aptitude install -y --assume-yes -q git git-core subversion git-svn
+  sudo -E aptitude install -y --assume-yes -q python-software-properties
+  sudo -E add-apt-repository -y ppa:git-core/ppa
+  sudo -E aptitude update -y --assume-yes -q
+  sudo -E aptitude upgrade -y --assume-yes -q
+  sudo -E aptitude install -y --assume-yes -q git git-core subversion git-svn
 fi
 
 ######################################################################################
@@ -125,7 +125,7 @@ echo -e "PROVISIONING: Installing Postfix and related mail stuff.\n"
 # Install postfix and general mail stuff.
 debconf-set-selections <<< "postfix postfix/mailname string vagrant.local"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
-sudo aptitude install -y --assume-yes -q postfix mailutils
+sudo -E aptitude install -y --assume-yes -q postfix mailutils
 
 ######################################################################################
 # UMASK
@@ -136,13 +136,13 @@ echo -e "PROVISIONING: Adjusting the UMASK stuff.\n"
 # Set the default UMASK value in 'login.defs' to be 002 instead of 022.
 LOGIN_DEFS_PATH="/etc/login.defs";
 if [ -f "${LOGIN_DEFS_PATH}" ]; then
-  sudo sed -i 's/UMASK[ \t]*022/UMASK\t\t002/g' "${LOGIN_DEFS_PATH}";
+  sudo -E sed -i 's/UMASK[ \t]*022/UMASK\t\t002/g' "${LOGIN_DEFS_PATH}";
 fi
 
 # Set the default UMASK value in 'common-session' to be 002 instead of 022.
 COMMON_SESSION_PATH="/etc/pam.d/common-session";
 if [ -f "${COMMON_SESSION_PATH}" ]; then
-  sudo sed -i 's/^session optional[ \t]*pam_umask\.so$/session\soptional\t\tpam_umask\.so\t\tumask=0002/g' "${COMMON_SESSION_PATH}";
+  sudo -E sed -i 's/^session optional[ \t]*pam_umask\.so$/session\soptional\t\tpam_umask\.so\t\tumask=0002/g' "${COMMON_SESSION_PATH}";
 fi
 
 ######################################################################################
@@ -155,7 +155,7 @@ echo -e "PROVISIONING: SSH adjustments.\n"
 SSH_CONFIG_PATH="/etc/ssh/ssh_config";
 if [ -f "${SSH_CONFIG_PATH}" ]; then
   SSH_APPEND="    PreferredAuthentications publickey,password,gssapi-with-mic,hostbased,keyboard-interactive";
-  sudo grep -q -F "${SSH_APPEND}" "${SSH_CONFIG_PATH}" || echo "${SSH_APPEND}" >> "${SSH_CONFIG_PATH}";
+  sudo -E grep -q -F "${SSH_APPEND}" "${SSH_CONFIG_PATH}" || echo "${SSH_APPEND}" >> "${SSH_CONFIG_PATH}";
 fi
 
 ######################################################################################
@@ -171,12 +171,12 @@ figlet "Vagrant" > "${MOTD_PATH}";
 echo "" >> "${MOTD_PATH}";
 
 # Disable these MOTD scripts.
-sudo chmod -f -x "/etc/update-motd.d/50-landscape-sysinfo";
-sudo chmod -f -x "/etc/update-motd.d/51-cloudguest";
-sudo chmod -f -x "/etc/update-motd.d/90-updates-available";
-sudo chmod -f -x "/etc/update-motd.d/91-release-upgrade";
-sudo chmod -f -x "/etc/update-motd.d/95-hwe-eol";
-sudo chmod -f -x "/etc/update-motd.d/98-cloudguest";
+sudo -E chmod -f -x "/etc/update-motd.d/50-landscape-sysinfo";
+sudo -E chmod -f -x "/etc/update-motd.d/51-cloudguest";
+sudo -E chmod -f -x "/etc/update-motd.d/90-updates-available";
+sudo -E chmod -f -x "/etc/update-motd.d/91-release-upgrade";
+sudo -E chmod -f -x "/etc/update-motd.d/95-hwe-eol";
+sudo -E chmod -f -x "/etc/update-motd.d/98-cloudguest";
 
 ######################################################################################
 # IPTables and IPSet
@@ -187,23 +187,23 @@ echo -e "PROVISIONING: IPTables and IPSet stuff.\n"
 # Install IPTables and IPSet stuff.
 debconf-set-selections <<< "iptables-persistent iptables-persistent/autosave_v4 boolean true"
 debconf-set-selections <<< "iptables-persistent iptables-persistent/autosave_v6 boolean true"
-sudo aptitude install -y --assume-yes -q iptables iptables-persistent ipset
+sudo -E aptitude install -y --assume-yes -q iptables iptables-persistent ipset
 
 # Load the IPSet stuff if the file exists.
 if [ -f "ipset.conf" ]; then
-  sudo ipset restore < "ipset.conf"
-  sudo cp "ipset.conf" "/etc/iptables/rules.ipsets"
+  sudo -E ipset restore < "ipset.conf"
+  sudo -E cp "ipset.conf" "/etc/iptables/rules.ipsets"
 fi
 
 # Load the IPTables stuff if the file exists.
 if [ -f "iptables.conf" ]; then
-  sudo iptables-restore < "iptables.conf"
-  sudo cp "iptables.conf" "/etc/iptables/rules.v4"
+  sudo -E iptables-restore < "iptables.conf"
+  sudo -E cp "iptables.conf" "/etc/iptables/rules.v4"
 fi
 
 # Patch 'iptables-persistent' if the patch exists and the original 'iptables-persistent' exists.
 if [ -f "/etc/init.d/iptables-persistent" ] && [ -f "iptables-persistent-ipset.patch" ]; then
-  sudo patch -fsb "/etc/init.d/iptables-persistent" < "iptables-persistent-ipset.patch"
+  sudo -E patch -fsb "/etc/init.d/iptables-persistent" < "iptables-persistent-ipset.patch"
 fi
 
 ######################################################################################
@@ -213,22 +213,22 @@ fi
 echo -e "PROVISIONING: Installing Apache and PHP stuff.\n"
 
 # Install the base Apache stuff.
-sudo aptitude install -y --assume-yes -q \
+sudo -E aptitude install -y --assume-yes -q \
   apache2 apache2-threaded-dev php5 \
   libapache2-mod-php5 php-pear
 
 # Install other PHP related stuff.
-sudo aptitude install -y --assume-yes -q \
+sudo -E aptitude install -y --assume-yes -q \
   php5-mysql php5-pgsql php5-odbc php5-sybase php5-sqlite \
   php5-xmlrpc php5-json php5-xsl php5-curl php5-geoip \
   php-getid3 php5-imap php5-ldap php5-mcrypt \
   php5-pspell php5-gmp php5-gd
 
 # Enable the PHP mcrypt module.
-sudo php5enmod mcrypt
+sudo -E php5enmod mcrypt
 
 # Enable these core Apache modules.
-sudo a2enmod -q rewrite headers expires include proxy proxy_http cgi
+sudo -E a2enmod -q rewrite headers expires include proxy proxy_http cgi
 
 ######################################################################################
 # Apache and PHP (Configuring)
@@ -240,54 +240,69 @@ echo -e "PROVISIONING: Configuring the Apache and PHP stuff.\n"
 PHP_CONFIG_PATH="/etc/php5/apache2/php.ini";
 if [ -f "${PHP_CONFIG_PATH}" ]; then
   # Harden PHP by disabling 'expose_php'.
-  sudo sed -i 's/expose_php = On/expose_php = Off/g' "${PHP_CONFIG_PATH}";
+  sudo -E sed -i 's/expose_php = On/expose_php = Off/g' "${PHP_CONFIG_PATH}";
   # Disable the PHP 5.5 opcache.
-  sudo sed -i 's/;opcache.enable=0/opcache.enable=0/g' "${PHP_CONFIG_PATH}";
+  sudo -E sed -i 's/;opcache.enable=0/opcache.enable=0/g' "${PHP_CONFIG_PATH}";
 fi
 
 # Harden Apache.
 APACHE_SECURITY_PATH="/etc/apache2/conf-available/security.conf";
 if [ -f "${APACHE_SECURITY_PATH}" ]; then
-  sudo sed -i 's/^ServerTokens OS/ServerTokens Prod/g' "${APACHE_SECURITY_PATH}";
-  sudo sed -i 's/^ServerSignature On/ServerSignature Off/g' "${APACHE_SECURITY_PATH}";
-  sudo sed -i 's/^TraceEnable On/TraceEnable Off/g' "${APACHE_SECURITY_PATH}";
+  sudo -E sed -i 's/^ServerTokens OS/ServerTokens Prod/g' "${APACHE_SECURITY_PATH}";
+  sudo -E sed -i 's/^ServerSignature On/ServerSignature Off/g' "${APACHE_SECURITY_PATH}";
+  sudo -E sed -i 's/^TraceEnable On/TraceEnable Off/g' "${APACHE_SECURITY_PATH}";
 fi
 
 # Adjust the Apache run group and UMASK.
 APACHE_ENVVARS_PATH="/etc/apache2/envvars";
 if [ -f "${APACHE_ENVVARS_PATH}" ]; then
-  sudo sed -i 's/^export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=www-readwrite/g' "${APACHE_ENVVARS_PATH}";
+  sudo -E sed -i 's/^export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=www-readwrite/g' "${APACHE_ENVVARS_PATH}";
   APACHE_APPEND="umask 002";
-  sudo grep -q -F "${APACHE_APPEND}" "${APACHE_ENVVARS_PATH}" || echo -e "\n${APACHE_APPEND}" >> "${APACHE_ENVVARS_PATH}";
+  sudo -E grep -q -F "${APACHE_APPEND}" "${APACHE_ENVVARS_PATH}" || echo -e "\n${APACHE_APPEND}" >> "${APACHE_ENVVARS_PATH}";
 fi
 
 # Set the config files for Apache.
-sudo cp "000-default.conf" "/etc/apache2/sites-available/000-default.conf"
-sudo cp "common.conf" "/etc/apache2/sites-available/common.conf"
+sudo -E cp "000-default.conf" "/etc/apache2/sites-available/000-default.conf"
+sudo -E cp "common.conf" "/etc/apache2/sites-available/common.conf"
 
 # Ditch the default Apache directory and set a new default page.
-sudo rm -rf "/var/www/html"
-sudo cp "index.php" "/var/www/index.php"
+sudo -E rm -rf "/var/www/html"
+sudo -E cp "index.php" "/var/www/index.php"
 
 # Replace the default man Apache config and the MPM prefork config with something simpler and more resource friendly.
-sudo cp "apache2.conf" "/etc/apache2/apache2.conf"
-sudo cp "mpm_prefork.conf" "/etc/apache2/mods-available/mpm_prefork.conf"
+sudo -E cp "apache2.conf" "/etc/apache2/apache2.conf"
+sudo -E cp "mpm_prefork.conf" "/etc/apache2/mods-available/mpm_prefork.conf"
 
 # Gracefully restart Apache to get the new config settings loaded.
-sudo service apache2 graceful
+sudo -E service apache2 graceful
 
 ######################################################################################
 # Apache Logs
 ######################################################################################
 
+echo -e "PROVISIONING: Adjusting Apache log rotation.\n"
+
 # Adjust the Apache log rotation script.
 APACHE_LOGROTATE_PATH="/etc/logrotate.d/apache2";
 if [ -f "${APACHE_SECURITY_PATH}" ]; then
-  sudo sed -i 's/rotate 52/rotate 13/g' "${APACHE_LOGROTATE_PATH}";
-  sudo sed -i 's/create 640 root adm/create 640 root www-readwrite/g' "${APACHE_LOGROTATE_PATH}";
+  sudo -E sed -i 's/rotate 52/rotate 13/g' "${APACHE_LOGROTATE_PATH}";
+  sudo -E sed -i 's/create 640 root adm/create 640 root www-readwrite/g' "${APACHE_LOGROTATE_PATH}";
 fi
 
 # Adjust permissions on log files.
-sudo chmod o+rx /var/log/apache2
-sudo chgrp www-readwrite /var/log/apache2/*
-sudo chmod 644 /var/log/apache2/*
+sudo -E chmod o+rx /var/log/apache2
+sudo -E chgrp www-readwrite /var/log/apache2/*
+sudo -E chmod 644 /var/log/apache2/*
+
+######################################################################################
+# MySQL
+######################################################################################
+
+echo -e "PROVISIONING: Installing and configuring MySQL stuff.\n"
+
+# Install the MySQL server and client.
+sudo -E aptitude install -y --assume-yes -q mysql-server mysql-client
+
+# Use this to prevent MySQL from coming up on reboot.
+sudo -E service mysql stop
+sudo -E update-rc.d -f mysql remove
