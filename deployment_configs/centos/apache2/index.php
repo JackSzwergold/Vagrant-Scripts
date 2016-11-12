0a0d5592da6f529bfaf6e774967891b081b33610
Jack Szwergold <JackSzwergold@gmail.com>
2016-11-12 00:58:54 -0500

    Adding a test to ensure that the server is creating files with correct permissions.

--- deployment_configs/centos/apache2/index.php
+++ deployment_configs/centos/apache2/index.php
@@ -6,4 +6,7 @@ echo $_SERVER['SERVER_NAME'];
 # echo '<br />';
 # echo $_SERVER['SERVER_ADDR'];
 
+# Test to ensure that the server is creating files with correct permissions.
+touch('zzz_test.txt');
+
 ?>
