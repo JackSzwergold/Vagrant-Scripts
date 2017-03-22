607ffda383f840daa57b23c872e60d3fcbcd5101
Jack Szwergold <JackSzwergold@gmail.com>
2017-03-22 11:34:25 -0400

    Tweaks to the default, text 'index.php' file.

--- deployment_configs/centos/httpd-centos-7/index.php
+++ deployment_configs/centos/httpd-centos-7/index.php
@@ -1,10 +1,10 @@
 <?php
 
-# phpinfo();
-# header('Location: http://www.preworn.com/');
 echo $_SERVER['SERVER_NAME'];
-# echo '<br />';
-# echo $_SERVER['SERVER_ADDR'];
+echo '<br />';
+echo $_SERVER['SERVER_ADDR'];
+echo '<br />';
+phpinfo();
 
 # Test to ensure that the server is creating files with correct permissions.
 # touch('zzz_test.txt');
