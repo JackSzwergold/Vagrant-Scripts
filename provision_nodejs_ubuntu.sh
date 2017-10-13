55bdfc5f44033964c3ae57498aa155f42ec9411e
Jack Szwergold <JackSzwergold@gmail.com>
2017-10-13 16:26:15 -0400

    Yet even more tweaks and cleanup.

--- provision_nodejs_ubuntu.sh
+++ provision_nodejs_ubuntu.sh
@@ -34,42 +34,42 @@
 
 BASE_DIR=$(pwd);
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Base directory is: '${BASE_DIR}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Base directory is: '${BASE_DIR}'.\033[0m";
 
 CONFIG_DIR="deployment_configs";
 if [ -n "$1" ]; then CONFIG_DIR="${1}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Config directory is: '${CONFIG_DIR}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Config directory is: '${CONFIG_DIR}'.\033[0m";
 
 DB_DIR="deployment_dbs";
 if [ -n "$2" ]; then DB_DIR="${2}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: DB directory is: '${DB_DIR}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: DB directory is: '${DB_DIR}'.\033[0m";
 
 BINARIES_DIR="deployment_binaries";
 if [ -n "$3" ]; then BINARIES_DIR="${3}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Binaries directory is: '${BINARIES_DIR}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Binaries directory is: '${BINARIES_DIR}'.\033[0m";
 
 USER_NAME="vagrant";
 if [ -n "$4" ]; then USER_NAME="${4}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: User name is: '${USER_NAME}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: User name is: '${USER_NAME}'.\033[0m";
 
 PASSWORD="vagrant";
 if [ -n "$5" ]; then PASSWORD="${5}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: User password is: '${PASSWORD}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: User password is: '${PASSWORD}'.\033[0m";
 
 MACHINE_NAME="vagrant";
 if [ -n "$6" ]; then MACHINE_NAME="${6}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Machine name is: '${MACHINE_NAME}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Machine name is: '${MACHINE_NAME}'.\033[0m";
 
 HOST_NAME="vagrant.local";
 if [ -n "$7" ]; then HOST_NAME="${7}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Host name is: '${HOST_NAME}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Host name is: '${HOST_NAME}'.\033[0m";
 
 ##########################################################################################
 # Go into the config directory.
@@ -82,7 +82,7 @@ cd "${BASE_DIR}/${CONFIG_DIR}";
 ##########################################################################################
 
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Setting the Debian frontend to non-interactive mode.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Setting the Debian frontend to non-interactive mode.\033[0m";
 export DEBIAN_FRONTEND=noninteractive;
 
 ##########################################################################################
@@ -100,7 +100,7 @@ export DEBIAN_FRONTEND=noninteractive;
 function configure_user_and_group () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Adjusting user and group related items.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Adjusting user and group related items.\033[0m";
 
   # Create the 'www-readwrite' group.
   sudo -E groupadd -f www-readwrite;
@@ -122,10 +122,13 @@ function configure_user_and_group () {
 function install_aptitude () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Install Aptitude.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Install Aptitude.\033[0m";
 
   # Install Aptitude.
-  sudo -E apt install -y -q=2 aptitude;
+  sudo -E apt-get install -y -q=2 aptitude aptitude-common;
+
+  # Update Aptitude.
+  sudo -E aptitude -y -q=2 update;
 
 } # install_aptitude
 
@@ -138,7 +141,7 @@ function set_environment () {
   cd "${BASE_DIR}/${CONFIG_DIR}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Setting the selected editor.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Setting the selected editor.\033[0m";
 
   # Set the selected editor to be Nano.
   if [ ! -f "${BASE_DIR}/.selected_editor" ]; then
@@ -147,7 +150,7 @@ function set_environment () {
   fi
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Importing the crontab.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Importing the crontab.\033[0m";
 
   # Importing the crontab.
   sudo -E crontab < "crontab.conf";
@@ -161,9 +164,21 @@ function set_timezone () {
 
   TIMEZONE="America/New_York";
 
-  echo -e "\033[33;1mPROVISIONING: Setting timezone data.\033[0m\n";
+  # Output a provisioning message.
+  echo -e "\033[33;1mPROVISIONING: Setting timezone data.\033[0m";
 
+  # Set the timezone.
   sudo -E timedatectl set-timezone "${TIMEZONE}";
+
+  # Do this stuff to get NTP setup.
+  sudo -E service ntp stop;
+  sudo -E ntpd -gq;
+  sudo service ntp start;
+  # sudo -E update-rc.d -f ntp defaults;
+  sudo -E update-rc.d -f ntp enable;
+
+  # Set the NTP synchronized value to 'true'.
+  sudo timedatectl set-ntp true;
 
 } # set_timezone
 
@@ -177,7 +192,7 @@ function configure_sources_list () {
   if [ -f "${SOURCES_LIST}" ] && grep -E -q "${DEB_URL_PATTERN}" "/etc/apt/sources.list"; then
 
     # Output a provisioning message.
-    echo -e "\033[33;1mPROVISIONING: Adjusting the sources list.\033[0m\n";
+    echo -e "\033[33;1mPROVISIONING: Adjusting the sources list.\033[0m";
 
     # Adjust the sources list.
     sudo -E sed -i "/${DEB_URL_PATTERN}/s/^# //g" "/etc/apt/sources.list";
@@ -192,10 +207,10 @@ function configure_sources_list () {
 function install_avahi () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Avahi related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Avahi related stuff.\033[0m";
 
   # Install Avahi.
-  sudo -E aptitude install -y -q=2 avahi-daemon avahi-utils;
+  sudo -E aptitude -y -q=2 install avahi-daemon avahi-utils;
 
 } # install_avahi
 
@@ -208,10 +223,10 @@ function install_sysstat () {
   cd "${BASE_DIR}/${CONFIG_DIR}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Sysstat related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Sysstat related stuff.\033[0m";
 
   # Install Sysstat.
-  sudo -E aptitude install -y -q=2 sysstat;
+  sudo -E aptitude -y -q=2 install sysstat;
 
   # Copy the Sysstat config file in place and restart sysstat.
   if [ -f "sysstat/sysstat" ]; then
@@ -227,14 +242,14 @@ function install_sysstat () {
 function install_basic_tools () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing a set of generic tools.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing a set of generic tools.\033[0m";
 
   # Install generic tools.
-  sudo -E aptitude install -y -q=2 \
+  sudo -E aptitude -y -q=2 install \
     dnsutils traceroute nmap bc htop finger curl whois rsync lsof \
     iftop figlet lynx mtr-tiny iperf nload zip unzip attr sshpass \
-    dkms mc elinks ntp dos2unix p7zip-full nfs-common \
-    slurm sharutils uuid-runtime chkconfig quota pv trickle apachetop \
+    dkms mc elinks dos2unix p7zip-full nfs-common \
+    slurm sharutils uuid-runtime quota pv trickle ntp \
     virtualbox-dkms;
 
 } # install_basic_tools
@@ -245,10 +260,10 @@ function install_basic_tools () {
 function install_locate () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing the locate tool and updating the database.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing the locate tool and updating the database.\033[0m";
 
   # Install Locate.
-  sudo -E aptitude install -y -q=2 mlocate;
+  sudo -E aptitude -y -q=2 install mlocate;
 
   # Update Locate.
   sudo -E updatedb;
@@ -261,10 +276,10 @@ function install_locate () {
 function install_compiler () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing the core compiler tools.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing the core compiler tools.\033[0m";
 
   # Install the core compiler and build tools.
-  sudo -E aptitude install -y -q=2 build-essential libtool;
+  sudo -E aptitude -y -q=2 install build-essential libtool automake m4;
 
 } # install_compiler
 
@@ -274,16 +289,16 @@ function install_compiler () {
 function install_git () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing Git and related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing Git and related stuff.\033[0m";
 
   # Purge any already installed version of Git.
   sudo -E aptitude purge -y -q git git-core subversion git-svn;
 
   # Now install Git via PPA.
-  sudo -E aptitude install -y -q=2 python-software-properties;
+  sudo -E aptitude -y -q=2 install python-software-properties;
   sudo -E add-apt-repository -y ppa:git-core/ppa;
-  sudo -E aptitude update -y -q=2;
-  sudo -E aptitude install -y -q=2 git git-core subversion git-svn;
+  sudo -E aptitude -y -q=2 update;
+  sudo -E aptitude -y -q=2 install git git-core subversion git-svn;
 
 } # install_git
 
@@ -296,10 +311,10 @@ function configure_motd () {
   cd "${BASE_DIR}/${CONFIG_DIR}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Setting the MOTD banner.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Setting the MOTD banner.\033[0m";
 
   # Install figlet.
-  sudo -E aptitude install -y -q=2 figlet;
+  sudo -E aptitude -y -q=2 install figlet;
 
   # Set the server login banner with figlet.
   MOTD_PATH="/etc/motd";
@@ -307,7 +322,7 @@ function configure_motd () {
   echo "" >> "${MOTD_PATH}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Disabling MOTD scripts.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Disabling MOTD scripts.\033[0m";
 
   # Disable these MOTD scripts.
   sudo -E chmod -f -x "/etc/update-motd.d/50-landscape-sysinfo";
@@ -325,10 +340,10 @@ function configure_motd () {
 function install_monit () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Monit related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Monit related stuff.\033[0m";
 
   # Install Monit.
-  sudo -E RUNLEVEL=1 aptitude install -y -q=2 monit;
+  sudo -E RUNLEVEL=1 aptitude -y -q=2 install monit;
 
   # Run these commands to prevent Monit from coming up on reboot.
   sudo -E service monit stop;
@@ -345,7 +360,7 @@ function configure_monit () {
   cd "${BASE_DIR}/${CONFIG_DIR}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing the Monit configs.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing the Monit configs.\033[0m";
 
   sudo -E cp -f "monit/monitrc" "/etc/monit/monitrc";
   sudo -E cp -f "monit/node_app.conf" "/etc/monit/conf.d/node_app.conf";
@@ -365,30 +380,29 @@ function configure_monit () {
 function install_nodejs () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing NodeJS and NPM related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing NodeJS and NPM related stuff.\033[0m";
 
   # Go into the base directory.
   cd "${BASE_DIR}";
 
   # Purge any already installed version of NodeJS and NPM.
-  sudo -E aptitude purge -y -q=2 node npm;
+  sudo -E aptitude -y -q=2 purge node npm;
 
   # Now install NodeJS and NPM via PPA.
-  sudo -E aptitude install -y -q=2 python-software-properties;
+  sudo -E aptitude -y -q=2 install python-software-properties;
   # curl -sL https://deb.nodesource.com/setup_6.x | sudo bash - ;
   # curl -sL https://deb.nodesource.com/setup_5.x | sudo bash - ;
   # curl -sL https://deb.nodesource.com/setup_4.x | sudo bash - ;
   # curl -sL https://deb.nodesource.com/setup_0.10 | sudo bash - ;
   curl -sL https://deb.nodesource.com/setup_4.x | sudo bash - ;
-  sudo -E aptitude update -y -q=2;
-  sudo -E aptitude install -y -q=2 nodejs;
+  sudo -E aptitude -y -q=2 update;
+  sudo -E aptitude -y -q=2 install nodejs;
 
   # Install 'forever' and 'userdown' for Upstart script support.
   sudo -E npm install -g --no-optional forever 2>&1 >/dev/null;
   sudo -E npm install -g --no-optional userdown 2>&1 >/dev/null;
 
 } # install_nodejs
-
 
 ##########################################################################################
 # Nginx
@@ -399,10 +413,10 @@ function install_nginx () {
   cd "${BASE_DIR}/${CONFIG_DIR}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing Nginx related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing Nginx related stuff.\033[0m";
 
   # Now install NodeJS and NPM via PPA.
-  sudo -E aptitude install -y -q=2 nginx-full;
+  sudo -E aptitude -y -q=2 install nginx-full;
 
   # Copy the Sysstat config file in place and restart sysstat.
   NGINX_CONF_PATH="/etc/nginx/sites-available";
@@ -420,7 +434,7 @@ function install_nginx () {
 function set_application_deployment_directories () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Creating the web code deployment directories.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Creating the web code deployment directories.\033[0m";
 
   sudo -E mkdir -p "/opt/webapps/"{builds,configs,deployed_code};
   sudo -E chown -f -R "${USER_NAME}":www-readwrite "/opt/webapps";
@@ -436,7 +450,7 @@ function set_application_deployment_directories () {
 function update_locate_db () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Updating the locate database.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Updating the locate database.\033[0m";
 
   sudo -E updatedb;
 
@@ -451,18 +465,23 @@ function update_locate_db () {
 #
 ##########################################################################################
 
+# Install install stuff.
 configure_user_and_group;
 install_aptitude;
 set_environment;
-set_timezone;
 configure_sources_list;
-hash avahi-daemon 2>/dev/null || { install_avahi; }
 hash sar 2>/dev/null || { install_sysstat; }
 hash updatedb 2>/dev/null || { install_locate; }
 configure_motd;
 install_basic_tools;
 hash libtool 2>/dev/null || { install_compiler; }
 if ! grep -q -s "git-core" "/etc/apt/sources.list" "/etc/apt/sources.list.d/"*; then install_git; fi
+
+# Timezone and related stuff.
+set_timezone;
+
+# Avahi
+hash avahi-daemon 2>/dev/null || { install_avahi; }
 
 # Install configure NodeJS and NPM.
 hash node 2>/dev/null || { install_nodejs; }
