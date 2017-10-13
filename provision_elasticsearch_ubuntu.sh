16390a97a321eac7ded1b1a4a24bc1c7ae25427a
Jack Szwergold <JackSzwergold@gmail.com>
2017-10-13 16:52:41 -0400

    Yet even more tweaks and cleanup.

--- provision_elasticsearch_ubuntu.sh
+++ provision_elasticsearch_ubuntu.sh
@@ -33,42 +33,42 @@
 
 BASE_DIR=$(pwd);
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Base directory is: '${BASE_DIR}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Base directory is: '${BASE_DIR}'.\033[0m";
 
 CONFIG_DIR="deployment_configs";
 if [ -n "$1" ]; then CONFIG_DIR="${1}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Config directory is: '${CONFIG_DIR}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Config directory is: '${CONFIG_DIR}'.\033[0m";
 
 DB_DIR="deployment_dbs";
 if [ -n "$2" ]; then DB_DIR="${2}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: DB directory is: '${DB_DIR}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: DB directory is: '${DB_DIR}'.\033[0m";
 
 BINARIES_DIR="deployment_binaries";
 if [ -n "$3" ]; then BINARIES_DIR="${3}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Binaries directory is: '${BINARIES_DIR}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Binaries directory is: '${BINARIES_DIR}'.\033[0m";
 
 USER_NAME="vagrant";
 if [ -n "$4" ]; then USER_NAME="${4}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: User name is: '${USER_NAME}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: User name is: '${USER_NAME}'.\033[0m";
 
 PASSWORD="vagrant";
 if [ -n "$5" ]; then PASSWORD="${5}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: User password is: '${PASSWORD}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: User password is: '${PASSWORD}'.\033[0m";
 
 MACHINE_NAME="vagrant";
 if [ -n "$6" ]; then MACHINE_NAME="${6}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Machine name is: '${MACHINE_NAME}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Machine name is: '${MACHINE_NAME}'.\033[0m";
 
 HOST_NAME="vagrant.local";
 if [ -n "$7" ]; then HOST_NAME="${7}"; fi
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Host name is: '${HOST_NAME}'.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Host name is: '${HOST_NAME}'.\033[0m";
 
 ##########################################################################################
 # Go into the config directory.
@@ -81,7 +81,7 @@ cd "${BASE_DIR}/${CONFIG_DIR}";
 ##########################################################################################
 
 # Output a provisioning message.
-echo -e "\033[33;1mPROVISIONING: Setting the Debian frontend to non-interactive mode.\033[0m\n";
+echo -e "\033[33;1mPROVISIONING: Setting the Debian frontend to non-interactive mode.\033[0m";
 export DEBIAN_FRONTEND=noninteractive;
 
 ##########################################################################################
@@ -99,7 +99,7 @@ export DEBIAN_FRONTEND=noninteractive;
 function configure_user_and_group () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Adjusting user and group related items.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Adjusting user and group related items.\033[0m";
 
   # Create the 'www-readwrite' group.
   sudo -E groupadd -f www-readwrite;
@@ -120,10 +120,14 @@ function configure_user_and_group () {
 ##########################################################################################
 function install_aptitude () {
 
-  echo -e "\033[33;1mPROVISIONING: Install Aptitude.\033[0m\n";
+  # Output a provisioning message.
+  echo -e "\033[33;1mPROVISIONING: Install Aptitude.\033[0m";
 
   # Install Aptitude.
-  sudo -E apt install -y -q=2 aptitude;
+  sudo -E apt-get -y -q=2 install aptitude aptitude-common;
+
+  # Update Aptitude.
+  sudo -E aptitude -y -q=2 update;
 
 } # install_aptitude
 
@@ -133,7 +137,7 @@ function install_aptitude () {
 function set_environment () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Setting the selected editor.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Setting the selected editor.\033[0m";
 
   # Set the selected editor to be Nano.
   if [ ! -f "${BASE_DIR}/.selected_editor" ]; then
@@ -142,7 +146,7 @@ function set_environment () {
   fi
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Importing the crontab.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Importing the crontab.\033[0m";
 
   # Importing the crontab.
   sudo -E crontab < "crontab.conf";
@@ -155,16 +159,22 @@ function set_environment () {
 function set_timezone () {
 
   TIMEZONE="America/New_York";
-  TIMEZONE_PATH="/etc/timezone";
-  if [ "${TIMEZONE}" != $(cat "${TIMEZONE_PATH}") ]; then
 
-    # Output a provisioning message.
-    echo -e "\033[33;1mPROVISIONING: Setting timezone data.\033[0m\n";
+  # Output a provisioning message.
+  echo -e "\033[33;1mPROVISIONING: Setting timezone data.\033[0m";
 
-    sudo -E echo "${TIMEZONE}" > "${TIMEZONE_PATH}";
-    sudo -E dpkg-reconfigure -f noninteractive tzdata 2>/dev/null;
+  # Set the timezone.
+  sudo -E timedatectl set-timezone "${TIMEZONE}";
 
-  fi
+  # Do this stuff to get NTP setup.
+  sudo -E service ntp stop;
+  sudo -E ntpd -gq;
+  sudo service ntp start;
+  # sudo -E update-rc.d -f ntp defaults;
+  sudo -E update-rc.d -f ntp enable;
+
+  # Set the NTP synchronized value to 'true'.
+  sudo -E timedatectl set-ntp true;
 
 } # set_timezone
 
@@ -178,7 +188,7 @@ function configure_sources_list () {
   if [ -f "${SOURCES_LIST}" ] && grep -E -q "${DEB_URL_PATTERN}" "/etc/apt/sources.list"; then
 
     # Output a provisioning message.
-    echo -e "\033[33;1mPROVISIONING: Adjusting the sources list.\033[0m\n";
+    echo -e "\033[33;1mPROVISIONING: Adjusting the sources list.\033[0m";
 
     # Adjust the sources list.
     sudo -E sed -i "/${DEB_URL_PATTERN}/s/^# //g" "/etc/apt/sources.list";
@@ -193,10 +203,10 @@ function configure_sources_list () {
 function install_avahi () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Avahi related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Avahi related stuff.\033[0m";
 
   # Install Avahi.
-  sudo -E aptitude install -y -q=2 avahi-daemon avahi-utils;
+  sudo -E aptitude -y -q=2 install avahi-daemon avahi-utils;
 
 } # install_avahi
 
@@ -209,10 +219,10 @@ function install_sysstat () {
   cd "${BASE_DIR}/${CONFIG_DIR}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Sysstat related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Sysstat related stuff.\033[0m";
 
   # Install Sysstat.
-  sudo -E aptitude install -y -q=2 sysstat;
+  sudo -E aptitude -y -q=2 install sysstat;
 
   # Copy the Sysstat config file in place and restart sysstat.
   if [ -f "sysstat/sysstat" ]; then
@@ -228,10 +238,10 @@ function install_sysstat () {
 function install_basic_tools () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing a set of generic tools.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing a set of generic tools.\033[0m";
 
   # Install generic tools.
-  sudo -E aptitude install -y -q=2 \
+  sudo -E aptitude -y -q=2 install \
     dnsutils traceroute nmap bc htop finger curl whois rsync lsof \
     iftop figlet lynx mtr-tiny iperf nload zip unzip attr sshpass \
     dkms mc elinks ntp dos2unix p7zip-full nfs-common \
@@ -246,10 +256,10 @@ function install_basic_tools () {
 function install_locate () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing the locate tool and updating the database.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing the locate tool and updating the database.\033[0m";
 
   # Install Locate.
-  sudo -E aptitude install -y -q=2 mlocate;
+  sudo -E aptitude -y -q=2 install mlocate;
 
   # Update Locate.
   sudo -E updatedb;
@@ -262,10 +272,10 @@ function install_locate () {
 function install_compiler () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing the core compiler tools.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing the core compiler tools.\033[0m";
 
   # Install the core compiler and build tools.
-  sudo -E aptitude install -y -q=2 build-essential libtool;
+  sudo -E aptitude -y -q=2 install build-essential libtool;
 
 } # install_compiler
 
@@ -275,16 +285,16 @@ function install_compiler () {
 function install_git () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing Git and related stuff.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing Git and related stuff.\033[0m";
 
   # Purge any already installed version of Git.
   sudo -E aptitude purge -y -q git git-core subversion git-svn;
 
   # Now install Git via PPA.
-  sudo -E aptitude install -y -q=2 python-software-properties;
+  sudo -E aptitude -y -q=2 install python-software-properties;
   sudo -E add-apt-repository -y ppa:git-core/ppa;
-  sudo -E aptitude update -y -q=2;
-  sudo -E aptitude install -y -q=2 git git-core subversion git-svn;
+  sudo -E aptitude -y -q=2 update;
+  sudo -E aptitude -y -q=2 install git git-core subversion git-svn;
 
 } # install_git
 
@@ -294,10 +304,10 @@ function install_git () {
 function configure_motd () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Setting the MOTD banner.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Setting the MOTD banner.\033[0m";
 
   # Install figlet.
-  sudo -E aptitude install -y -q=2 figlet;
+  sudo -E aptitude -y -q=2 install figlet;
 
   # Set the server login banner with figlet.
   # MOTD_PATH="/etc/motd.tail";
@@ -307,7 +317,7 @@ function configure_motd () {
   echo "" >> "${MOTD_PATH}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Disabling MOTD scripts.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Disabling MOTD scripts.\033[0m";
 
   # Disable these MOTD scripts.
   sudo -E chmod -f -x "/etc/update-motd.d/50-landscape-sysinfo";
@@ -325,15 +335,15 @@ function configure_motd () {
 function install_java () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing Java.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing Java.\033[0m";
 
   # Now install Java via PPA.
-  sudo -E aptitude install -y -q=2 python-software-properties debconf-utils;
+  sudo -E aptitude -y -q=2 install python-software-properties debconf-utils;
 
   sudo -E add-apt-repository ppa:webupd8team/java;
-  sudo -E aptitude update -y -q=2;
+  sudo -E aptitude -y -q=2 update;
   echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections;
-  sudo -E aptitude install -y -q=2 oracle-java8-installer oracle-java8-set-default;
+  sudo -E aptitude -y -q=2 install oracle-java8-installer oracle-java8-set-default;
 
   # Now install Java via PPA.
   echo "JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre" >> "/etc/environment";
@@ -346,13 +356,13 @@ function install_java () {
 function install_elasticsearch () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Installing ElasticSearch related items.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Installing ElasticSearch related items.\033[0m";
 
   # Import the public key used by the package management system:
   wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -;
   echo 'deb http://packages.elasticsearch.org/elasticsearch/1.7/debian stable main' | sudo tee /etc/apt/sources.list.d/elasticsearch.list;
-  sudo -E aptitude update -y -q=2;
-  sudo -E RUNLEVEL=1 aptitude install -y -q=2 elasticsearch;
+  sudo -E aptitude -y -q=2 update;
+  sudo -E RUNLEVEL=1 aptitude -y -q=2 install elasticsearch;
 
   # Set ElasticSearch to be able to come up on reboot.
   sudo update-rc.d elasticsearch defaults 95 10;
@@ -371,7 +381,7 @@ function configure_elasticsearch () {
   cd "${BASE_DIR}/${CONFIG_DIR}";
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Configuring ElasticSearch related items.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Configuring ElasticSearch related items.\033[0m";
 
   # Copy the Elasticsearch config file in place and restart sysstat.
   if [ -f "elasticsearch/elasticsearch.yml" ]; then
@@ -387,7 +397,7 @@ function configure_elasticsearch () {
 function update_locate_db () {
 
   # Output a provisioning message.
-  echo -e "\033[33;1mPROVISIONING: Updating the locate database.\033[0m\n";
+  echo -e "\033[33;1mPROVISIONING: Updating the locate database.\033[0m";
 
   sudo -E updatedb;
 
