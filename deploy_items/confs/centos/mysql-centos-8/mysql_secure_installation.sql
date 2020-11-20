4ff2a39308804e43942ec5fc0c7055bfda176bf8
Jack Szwergold <JackSzwergold@gmail.com>
2020-11-20 13:10:42 -0500

    MySQL 8 does not allow for easy altering of the root password from a script as in the past versions so just commenting that stuff out for now.

--- deploy_items/confs/centos/mysql-centos-8/mysql_secure_installation.sql
+++ deploy_items/confs/centos/mysql-centos-8/mysql_secure_installation.sql
@@ -1,7 +1,9 @@
 /* This script does the equivalent of 'mysql_secure_installation'. */
 
 UPDATE mysql.user SET plugin='' WHERE User='root';
-UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
+-- UPDATE mysql.user SET Password = PASSWORD('root') WHERE User = 'root';
+-- UPDATE mysql.user SET Password = CONCAT('*', UPPER(SHA1(UNHEX(SHA1('root'))))) WHERE User = 'root';
+-- ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
