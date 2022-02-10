3debc1e06ff9da2905d630af5011b7d2eead6b00
Jack Szwergold <JackSzwergold@gmail.com>
2022-02-10 16:07:47 -0500

    Setting the PHP install to PHP 7.4.

--- provision_centos_7.sh
+++ provision_centos_7.sh
@@ -417,8 +417,8 @@ function install_apache () {
   # Install the Yum utils.
   sudo -E yum install --nogpgcheck -y -q -e 0 yum-utils;
 
-  # Set the Yum config manager to the REMI PHP 7.2 version.
-  sudo -E yum-config-manager -y -q -e 0 --enable remi-php73;
+  # Set the Yum config manager to the Remi repo PHP 7.4 version.
+  sudo -E yum-config-manager -y -q -e 0 --enable remi-php74;
 
   # Install the base Apache related items.
   sudo -E yum install --nogpgcheck -y -q -e 0 httpd httpd-devel \
